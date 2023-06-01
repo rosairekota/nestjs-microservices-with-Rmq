d6ae4f3f2487d455adb962240d4458b08dcbfd8b
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-06-01 09:16:30 +0100

    feat: add mongo db images

--- apps/subscription/src/subscription.module.ts
+++ apps/subscription/src/subscription.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 import { DatabaseModule } from '@app/database';
+import { SubscriptionRepisitory } from './repository/subscription.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Subscription, SubscriptionSchema } from './schema/subscription.schema';
 
 @Module({
   imports: [
@@ -11,12 +14,19 @@ import { DatabaseModule } from '@app/database';
       isGlobal: true,
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
       }),
       envFilePath: './apps/subscription/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([
+      {
+        name: Subscription.name,
+        schema: SubscriptionSchema,
+      },
+    ]),
   ],
   controllers: [SubscriptionController],
-  providers: [SubscriptionService],
+  providers: [SubscriptionService, SubscriptionRepisitory],
 })
 export class SubscriptionModule {}
