ce0baa7f0f1414764e2a948100e7f8d2e9eba49f
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-06-01 06:01:19 +0100

    feat: add Momgoose config

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [DatabaseService],
   exports: [DatabaseService],
 })
