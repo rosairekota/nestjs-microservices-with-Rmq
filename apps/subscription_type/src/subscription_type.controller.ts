4ce605ba747888cdf1413019f2e754ddd1a5ceb2
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-06-05 11:46:55 +0100

    feat: add emit data

--- apps/subscription_type/src/subscription_type.controller.ts
+++ apps/subscription_type/src/subscription_type.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { SubscriptionTypeService } from './subscription_type.service';
+import { EventPattern, Ctx, Payload, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class SubscriptionTypeController {
-  constructor(private readonly subscriptionTypeService: SubscriptionTypeService) {}
+  constructor(
+    private readonly subscriptionTypeService: SubscriptionTypeService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.subscriptionTypeService.getHello();
+  }
+  @EventPattern('subscription_created')
+  handleSubscriptionCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    this.subscriptionTypeService.create(data)
   }
 }
--- apps/subscription_type/src/subscription_type.service.ts
+++ apps/subscription_type/src/subscription_type.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class SubscriptionTypeService {
+  private logger: Logger = new Logger(SubscriptionTypeService.name);
   getHello(): string {
     return 'Hello World!';
+  }
+  create(data: any) {
+    this.logger.log('events emitted: ', data);
   }
 }
